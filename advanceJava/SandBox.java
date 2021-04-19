c007ee613b7e923310b8ab8be0acf4be56dfa6b7
Tim Kao <pakuya.kao@gmail.com>
2021-04-19 15:17:22 -0400

    completableFuture is like promise in JavaScript

--- advanceJava/SandBox.java
+++ advanceJava/SandBox.java
@@ -6,14 +6,19 @@ public class SandBox {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
-				
-		CompletableFuture.supplyAsync(() -> longNetworkProcess(2000)).thenAccept(value -> System.out.println("complete " + value ));
-		try {
+		CompletableFuture<Integer> cf = CompletableFuture.supplyAsync(() -> longNetworkProcess(20));
+		cf.thenAccept(value -> System.out.println("the result is " + value));
+		// 如果执行异常:
+        cf.exceptionally((e) -> {
+            e.printStackTrace();
+            return null;
+        });
+        // 主线程不要立刻结束，否则CompletableFuture默认使用的线程池会立刻关闭:
+        try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System.out.println("A wake");
 	}
 	
 	public static int longNetworkProcess(int value) {
