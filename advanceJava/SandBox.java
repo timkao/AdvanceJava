be639694ae08271f2b7d8d4a4ce3e6f8c07c59f4
Tim Kao <pakuya.kao@gmail.com>
2021-04-19 15:34:36 -0400

    add a summary

--- advanceJava/SandBox.java
+++ advanceJava/SandBox.java
@@ -46,5 +46,13 @@ public class SandBox {
 		return value * 3;
 		
 	}
-
+	
+	/*
+	 * CompletableFuture可以指定异步处理流程：
+	 *   thenAccept()处理正常结果；
+	 *   exceptional()处理异常结果；
+	 *   thenApplyAsync()用于串行化另一个CompletableFuture；
+	 *   anyOf()和allOf()用于并行化多个CompletableFuture。
+	 * 
+	 * */
 }
